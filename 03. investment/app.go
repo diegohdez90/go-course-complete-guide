f734aec0fc71e148ebfd69ed3bc0ed3e749fe5f1
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2026-02-26 09:44:04 -0600

    feat(03. investment): add investment project

---
+++ 03. investment/app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var investmentAmount = 1000.0
+	var expectedResturnRate = 5.5
+	var years = 5
+
+	// In this code, investmentAmount is set as float64 because it represents a monetary value, which can have decimal places. Using float64 allows for more precision when dealing with financial calculations.
+	// On the other hand, years is an integer value that represents the number of years for the investment. However, when calculating the future value of the investment, we need to perform a multiplication operation that involves both the expected return rate and the number of years. To ensure that the calculation is accurate and does not lose precision, we cast years to float64 before performing the multiplication. This allows us to maintain the precision of the calculation and avoid any potential issues with integer division or rounding errors.
+	var futureValue = investmentAmount * math.Pow(1+expectedResturnRate/100, float64(years))
+	fmt.Println(futureValue)
+}
